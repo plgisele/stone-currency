30b40a41c6653442af8ae466c4eee246993aa3eb
Gisele Paiva <giselepaiva.arq@gmail.com>
2021-05-23 20:15:02 -0300

    adição da hora da cotação no header

--- script.js
+++ script.js
@@ -14,6 +14,26 @@ let cotacaoDia = JSON.parse(dolar);
 let valorDolar = cotacaoDia.USDBRL.bid;
 let dataCotacao = cotacaoDia.USDBRL.create_date;
 
+// Configuração da data da cotação
+function dataHora() {
+    dataCotacao = new Date();
+    let dia = dataCotacao.getDate();
+    let mes = dataCotacao.getMonth();
+    let ano = dataCotacao.getFullYear();
+    let hora = dataCotacao.getHours();
+    let minutos = dataCotacao.getMinutes();
+
+    let meses = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+
+    return dia + " de " + meses[mes] + " de " + ano + " | " + hora + ":" + minutos + " UTC"
+}
+
+let cotacaoDoDia = document.getElementById("data-cotacao");
+cotacaoDoDia.value = dataHora();
+
+
+//
+
 
 
 
